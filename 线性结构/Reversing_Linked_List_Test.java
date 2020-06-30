cd72cf0312cb2591ab04acae685a013bd3cee441
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-06-30 12:17:51 +0800

    增加分段单独判断+反转的函数，优化判断和前后逻辑

--- 线性结构/Reversing_Linked_List_Test.java
+++ 线性结构/Reversing_Linked_List_Test.java
@@ -1,5 +1,9 @@
-// site: http://www.jeepxie.net/article/269798.html
-//       https://blog.csdn.net/liyuanyue2017/article/details/83269991
+/* reference site:
+http://www.jeepxie.net/article/269798.html
+https://blog.csdn.net/liyuanyue2017/article/details/83269991
+https://blog.csdn.net/yeternity/article/details/72794956
+*/
+import java.rmi.UnexpectedException;
 import java.util.Scanner;
 
 class ArrayNode
@@ -34,10 +38,11 @@ public class Reversing_Linked_List_Test
         //获取真实链表的节点数量
         N = Numbers_true(arr, head_index);
         //每k个都反转(k>1)
-        if(K > 1)
+        /*if(K > 1)
         {
             head_index = LinkedListReverse(head_index, arr, N, K);
-        }
+        }*/
+        head_index = Reverse_LinkedList(arr, head_index, N, K);
         Display(head_index,arr);
     }
     public static int Numbers_true(ArrayNode[] arr, int head)
@@ -50,12 +55,15 @@ public class Reversing_Linked_List_Test
         }
         return n;
     }
+    /*
+    复杂+容易出错
+     */
     public static int LinkedListReverse(int head, ArrayNode[] arr, int N, int K)
     {
         int total_reverse = 1; //反转多少段链表
         int count = 1; //链表反转内部计数
         int save = head; //保存原链表的head(上一段的head/反转后新末尾)
-        int save_2 = head; //(下一段的head/反转后新末尾)
+        int save_2 = head;//(下一段的head/反转后新末尾)
         int temp; //反转过程保存中间next
         int new_head = head;
         int old_head = arr[head].next;
@@ -113,6 +121,78 @@ public class Reversing_Linked_List_Test
             System.out.println();
             head = arr[head].next;
         }
+    }
+
+    /*
+    反转整个链表
+    @return: 整个反转完之后的链表头结点位置
+     */
+    public static int Reverse_LinkedList(ArrayNode[] arr, int head, int N, int K)
+    {
+        int List_head; //整个链表的head位置
+        int Unreversed_head = head; //未反转的head位置
+        int Tail; //用于连接下一段逆转的链表
+        // 第一次先从head开始判断是不是需要反转
+        if(need_reverse(arr, Unreversed_head, K))
+        {
+            List_head = reverse_part(arr, Unreversed_head, K); //整个链表的头就是第一段反转完的头
+            Tail = Unreversed_head;
+            Unreversed_head = arr[Tail].next;
+        }
+        else //不需要反转
+        {
+            return head;
+        }
+        while(need_reverse(arr, Unreversed_head, K)) //从第二段开始
+        {
+            arr[Tail].next = reverse_part(arr, Unreversed_head, K); //上一段的表尾和下一段的表头连接
+            Tail = Unreversed_head; // 更新Tail，逆转前的表头就是逆转后的表尾，且新Tail已经接上了未逆转的部分
+            Unreversed_head = arr[Tail].next; //Tail的next就是下一段需要逆序的head
+        }
+        return List_head;
+    }
+
+    /*
+    统计从当前节点开始到null还有多少个节点，如果大于等于k就可以返回true
+     */
+    public static boolean need_reverse(ArrayNode[] arr, int start_pos, int K)
+    {
+        if(start_pos == -1)
+        {
+            return false;
+        }
+        for(int i = 1; arr[start_pos].next != -1; start_pos = arr[start_pos].next)
+        {
+            i++;
+            if(i == K)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+    从位置start_pos开始反转K个节点
+    @return: 反转后的新头节点位置new_head
+     */
+    public static int reverse_part(ArrayNode[] arr, int start_pos, int K)
+    {
+        int head = start_pos;
+        int new_head = head;
+        int old_head = arr[head].next;
+        int count = 1;
+        int temp;
+        while(count < K)
+        {
+            temp = arr[old_head].next;
+            arr[old_head].next = new_head;
+            new_head = old_head;
+            old_head = temp;
+            count ++;
+        }
+        arr[head].next = old_head; //原来的第一个节点逆转后变为最后一个节点，并连接剩下未逆转的元素
+        return new_head;
     }
 }
 /*
